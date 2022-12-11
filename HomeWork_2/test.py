ce942d2bcd085c5a1bef9bb699c0d4ff515bee1c
Igor <vorobiov.777@yandex.ru>
2022-12-11 23:25:59 +0300

    Updated HomeWork2.2

--- HomeWork_2/test.py
+++ HomeWork_2/test.py
@@ -1,9 +1,11 @@
-from nn import *
-from autograd import *
+# loss function
+import random
 import numpy as np
+from autograd import Value
+from nn import *
+
 
 def loss(X, y, model, batch_size=None):
-
     if batch_size is None:
         Xb, yb = X, y
     else:
@@ -15,21 +17,22 @@ def loss(X, y, model, batch_size=None):
     scores = list(map(model, inputs))
 
     # svm "max-margin" loss
-    losses = [(1 + -yi*scorei).relu() for yi, scorei in zip(yb, scores)]
+    losses = [(1 + -yi * scorei).relu() for yi, scorei in zip(yb, scores)]
     data_loss = sum(losses) * (1.0 / len(losses))
     # L2 regularization
     alpha = 1e-4
-    reg_loss = alpha * sum((p*p for p in model.parameters()))
+    reg_loss = alpha * sum((p * p for p in model.parameters()))
     total_loss = data_loss + reg_loss
 
     # also get accuracy
-    accuracy = [((yi).__gt__(0)) == ((scorei.data).__gt__(0)) for yi, scorei in zip(yb, scores)]
+    accuracy = [((yi).__gt__(0)) == (scorei.data.__gt__(0)) for yi, scorei in zip(yb, scores)]
     return total_loss, sum(accuracy) / len(accuracy)
+# optimization
 
 
-nn = MLP(3, [4, 4, 1])
-print(nn)
-print("number of parameters", len(nn.parameters()))
+model = MLP(3, [4, 4, 1])
+print(model)
+print("number of parameters", len(model.parameters()))
 
 xs = [
   [2.0, 3.0, -1.0],
@@ -42,19 +45,16 @@ ys = [1.0, -1.0, -1.0, 1.0] # desired targets
 for k in range(20):
 
     # forward
-    total_loss, acc = loss(xs, ys, nn)
+    total_loss, acc = loss(xs, ys, model)
 
-    # calculate loss (mean square error)
-    nn.zero_grad()
+    # backward
+    model.zero_grad()
     total_loss.backward()
 
-    # backward (zero_grad + backward)
-    ...
-
-    # update
-    learning_rate = 1.0 - 0.9*k/100
-    for p in nn.parameters():
+    # update (sgd)
+    learning_rate = 1.0 - 0.9 * k / 100
+    for p in model.parameters():
         p.data -= learning_rate * p.grad
 
     if k % 1 == 0:
-        print(f"step {k} loss {total_loss.data}, accuracy {acc*100}%")
+        print(f"step {k} loss {total_loss.data}, accuracy {acc * 100}%")
